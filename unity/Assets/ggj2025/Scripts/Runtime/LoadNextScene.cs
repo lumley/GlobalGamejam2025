bd1fedd4a0823da309d3574e0212fecf30fbe655
Luis Rojas <luis_andres81@hotmail.com>
2025-01-26 17:47:50 +0100

    Final cutscene

--- unity/Assets/ggj2025/Scripts/Runtime/LoadNextScene.cs
+++ unity/Assets/ggj2025/Scripts/Runtime/LoadNextScene.cs
@@ -3,24 +3,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LoadNextScene : MonoBehaviour
-{
+public class LoadNextScene : MonoBehaviour {
     [SerializeField] private SceneInput MainSceneInput;
     [SerializeField] private Button StartButton;
+    public bool ChangeOnStart = false;
 
+    private void Start() {
+        if (ChangeOnStart) {
+            SceneManager.LoadScene(MainSceneInput.SceneIndex);
+        }
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void OnEnable()
-    {
-        StartButton.onClick.AddListener(OnClick);
+    private void OnEnable() {
+        StartButton?.onClick.AddListener(OnClick);
     }
 
-    void OnDisable()
-    {
-        StartButton.onClick.RemoveListener(OnClick);
+    private void OnDisable() {
+        StartButton?.onClick.RemoveListener(OnClick);
 
-    }  
-    void OnClick()
-    {
+    }
+    
+    private void OnClick() {
         SceneManager.LoadScene(MainSceneInput.SceneIndex);
     }
 }
