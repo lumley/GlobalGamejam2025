011aeb4ba584b4cff44cd58b9c52daeb636d90bf
Luis Rojas <luis_andres81@hotmail.com>
2025-01-26 01:08:32 +0100

    Battery update

--- unity/Assets/ggj2025/Scripts/Runtime/BatteryUpdater.cs
+++ unity/Assets/ggj2025/Scripts/Runtime/BatteryUpdater.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 public class BatteryUpdater : MonoBehaviour {
     public string BatteryLevelVariableName = "BatteryLevel";
     public Image BatteryImage;
-    public Color GoodColor;
-    public Color BadColor;
-        
+    public Sprite[] Sprites;
+
     private void Update() {
         var batteryLevel = (int)Variables.Scene(gameObject.scene).Get(BatteryLevelVariableName);
-        BatteryImage.fillAmount = batteryLevel / 100f;
-        BatteryImage.color = batteryLevel > 20 ? GoodColor : BadColor;
+        var batteryLevelNormalized = batteryLevel / 100.0f;
+        var batterySpriteIndex = Mathf.FloorToInt(batteryLevelNormalized * (Sprites.Length - 1));
+        BatteryImage.sprite = Sprites[Mathf.Clamp(batterySpriteIndex, 0, Sprites.Length - 1)];
     }
 }
