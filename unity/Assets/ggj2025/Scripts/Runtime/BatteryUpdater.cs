e30e1801d1ce54def85afd9547e126d54672f1f3
Luis Rojas <luis_andres81@hotmail.com>
2025-01-26 15:17:00 +0100

    New final scenes, dead and victory

--- unity/Assets/ggj2025/Scripts/Runtime/BatteryUpdater.cs
+++ unity/Assets/ggj2025/Scripts/Runtime/BatteryUpdater.cs
@@ -1,8 +1,10 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class BatteryUpdater : MonoBehaviour {
+    public int DeadSceneIndex = 2;
     public string BatteryLevelVariableName = "BatteryLevel";
     public Image BatteryImage;
     public Sprite[] Sprites;
@@ -12,5 +14,9 @@ public class BatteryUpdater : MonoBehaviour {
         var batteryLevelNormalized = batteryLevel / 100.0f;
         var batterySpriteIndex = Mathf.FloorToInt(batteryLevelNormalized * (Sprites.Length - 1));
         BatteryImage.sprite = Sprites[Mathf.Clamp(batterySpriteIndex, 0, Sprites.Length - 1)];
+        if(batteryLevel <= 0) {
+            // Game over
+            SceneManager.LoadScene(DeadSceneIndex);
+        }
     }
 }
