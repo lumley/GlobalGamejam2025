cfd21d77c9f913d051494017bad224a01191e874
Oriana Laplana <>
2025-02-02 21:53:39 +0100

    Battery GFX curve

--- unity/Assets/ggj2025/Scripts/Runtime/BatteryUpdater.cs
+++ unity/Assets/ggj2025/Scripts/Runtime/BatteryUpdater.cs
@@ -8,11 +8,12 @@ public class BatteryUpdater : MonoBehaviour {
     public string BatteryLevelVariableName = "BatteryLevel";
     public Image BatteryImage;
     public Sprite[] Sprites;
+    public AnimationCurve Remap;
 
     private void Update() {
         var batteryLevel = (int)Variables.Scene(gameObject.scene).Get(BatteryLevelVariableName);
         var batteryLevelNormalized = batteryLevel / 100.0f;
-        var batterySpriteIndex = Mathf.FloorToInt(batteryLevelNormalized * (Sprites.Length - 1));
+        var batterySpriteIndex = Mathf.FloorToInt(Remap.Evaluate(batteryLevelNormalized) * (Sprites.Length - 1));
         BatteryImage.sprite = Sprites[Mathf.Clamp(batterySpriteIndex, 0, Sprites.Length - 1)];
         if(batteryLevel <= 0) {
             // Game over
